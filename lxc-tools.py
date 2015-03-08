8be0f667423152fef6aca696270d9c6692379a64
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-03-07 22:19:00 -0800

    execute function that kinda works + removing rando stuff

--- lxc-tools.py
+++ lxc-tools.py
@@ -1,5 +1,6 @@
 import lxc
-from subprocess import Popen
+import os
+from subprocess import Popen, call
 
 def get_running():
 	return [c for c in lxc.list_containers(as_object=True) if c.state == "RUNNING"]
@@ -37,4 +38,29 @@ def ssh_cluster(cs, user="root", local_user=None):
 	ssh = Popen(tcmd+["%s@%s" % (user, c.name) for c in cs])
 	ssh.wait()
 
-ssh_cluster(get_running(), local_user="rolands")
+def _run_command(args):
+    cmd, cwd = args
+    os.environ['PATH'] = '/usr/sbin:/usr/bin:/sbin:/bin'
+    os.environ['HOME'] = '/root'
+    return call(cmd, cwd=cwd)
+
+def run_script(container, script, cwd="/", interpreter="bash"):
+    rootfs = container.get_config_item("lxc.rootfs")
+    if rootfs.startswith("overlayfs:"):
+        raise ValueError("this doesn't work with overlay...")
+    cmdpath = "%s/tmp/exec_script" % rootfs
+    with open(cmdpath, "w+") as fd:
+        fd.write(script)
+    os.chmod(cmdpath, 0o755)
+    cmd = [interpreter, "/tmp/exec_script"]
+
+    print(" ==> Executing: \"%s\" in %s" % (" ".join(cmd), cwd))
+    retval = container.attach_wait(_run_command,
+                                   (cmd, cwd),
+                                   env_policy=lxc.LXC_ATTACH_CLEAR_ENV)
+
+    if retval != 0:
+        raise ValueError("Failed to run the command.")
+
+# ssh_cluster(get_running(), local_user="rolands")
+run_script(get_running()[0], "ls")
