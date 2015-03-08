4d00662f52068ce89c7187840a969581f993b254
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-03-07 22:55:18 -0800

    rename function

--- lxc-tools.py
+++ lxc-tools.py
@@ -2,17 +2,20 @@ import lxc
 import os
 from subprocess import Popen, call
 
+def error(msg, exit_code=0):
+    print("ERROR: %s" % (msg))
+    exit(exit_code)
+
 def get_running():
 	return [c for c in lxc.list_containers(as_object=True) if c.state == "RUNNING"]
 
 def get_stopped():
 	return [c for c in lxc.list_containers(as_object=True) if c.state == "STOPPED"]
 
-def stop(c):
-	if not c.shutdown(30):
+def stop(c, force=False):
+	if not c.shutdown(30) and force:
 		c.stop()
-		return False
-	return True
+	return not c.running
 
 def start(c):
 	if c.running:
@@ -47,7 +50,7 @@ def _run_command(args):
 def run_script(container, script, cwd="/", interpreter="bash"):
     rootfs = container.get_config_item("lxc.rootfs")
     if rootfs.startswith("overlayfs:"):
-        raise ValueError("this doesn't work with overlay...")
+       error("this doesn't work with overlay...")
     cmdpath = "%s/tmp/exec_script" % rootfs
     with open(cmdpath, "w+") as fd:
         fd.write(script)
@@ -60,7 +63,44 @@ def run_script(container, script, cwd="/", interpreter="bash"):
                                    env_policy=lxc.LXC_ATTACH_CLEAR_ENV)
 
     if retval != 0:
-        raise ValueError("Failed to run the command.")
+        error("Failed to run the command.")
+
+def rename(container, new_name, force_shutdown=False, restart=True):
+    sed_rpl_cmd = ["sed", "-i", "'s/%s/%s/g'" % (container.name, new_name), "/etc/hostname"]]
+
+    # before we do anything, make sure these isn't already a container duh
+    if any([new_name == c.name for c in lxc.list_containers(as_object=True)]):
+        eror("A container with this name already exists!")
+
+    current_path = "%s/%s" % (container.get_config_path(), container.name)
+    new_path = "%s/%s" % (container.get_config_path(), new_name)
+
+    # set hostname
+    if not container.running:
+        if not start(container):
+            error("couldn't start container %s" % (container.name))
+    container.attach_wait(_run_command, (sed_rpl_cmd, "/"), env_policy=lxc.LXC_ATTACH_CLEAR_ENV)
+
+    # shutdown container before moving
+    if container.running:
+        if not stop(container, force=force_shutdown):
+            error("container %s is still running [force_shutdown: %s]" % (container.name, force_shutdown))
+
+    # set config name
+    container.set_config_item("lxc.rootfs", container.get_config_item("lxc.roofs").replace(container.name, new_name))
+    container.set_config_item("lxc.mount", container.get_config_item("lxc.mount").replace(container.name, new_name))
+    container.set_config_item("lxc.utsname", new_name)
+
+    # move container to new home
+    shutil.move(current_path, new_path)
+
+    # restart container if ya want
+    if restart:
+        if not start(container):
+            error("couldn't start container %s" % (container.name))
 
 # ssh_cluster(get_running(), local_user="rolands")
-run_script(get_running()[0], "ls")
+# run_script(get_running()[0], "ls")
+# get_running()[0].attach_wait(_run_command, (["ls"], "/"), env_policy=lxc.LXC_ATTACH_CLEAR_ENV)
+
+
